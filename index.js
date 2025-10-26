3f947d3c5ef828108e1d79a6f4b3603891259860
moodles <rubbersoul11@163.com>
2025-10-26 23:25:00 +0800

    finish exercise 3.15-3.18

--- index.js
+++ index.js
@@ -44,26 +44,35 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-  const date = new Date()
-  res.send(`<div>Phonebook has info for ${persons.length} people</div>
-    ${date}`)
+  Person.find({}).then((persons) => {
+    const count = persons.length
+    const date = new Date()
+    res.send(`<div>Phonebook has info for ${count} people</div>
+    <div>${date}</div>`)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const personId = Number(req.params.id)
-  const person = persons.find((person) => person.id === personId)
-  if (!person) {
-    res.status(204).end()
-  } else {
-    res.json(person)
-  }
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const personId = Number(req.params.id)
-  persons = persons.filter((person) => person.id !== personId)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 // Configure middleware Morgan to log person infomation
@@ -105,9 +114,38 @@ app.post('/api/persons', (req, res) => {
   person.save().then((savedPersons) => {
     res.json(savedPersons)
   })
-
-  res.json(persons)
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (!person) {
+        return res.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        res.json(updatedPerson)
+      })
+    })
+    .catch((error) => next(error))
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send('malformatted id')
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
