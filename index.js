e8d6b2d46f2ffd16ac3b0fc01b9aad1c5b8be637
moodles <rubbersoul11@163.com>
2025-10-27 15:51:53 +0800

    finish exercise 3.19-3.21

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function isInPhonebook(name) {
   return names.includes(name)
 }
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body.name) {
@@ -111,9 +111,12 @@ app.post('/api/persons', (req, res) => {
     number: body.number,
   })
 
-  person.save().then((savedPersons) => {
-    res.json(savedPersons)
-  })
+  person
+    .save()
+    .then((savedPersons) => {
+      res.json(savedPersons)
+    })
+    .catch((error) => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -140,6 +143,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send('malformatted id')
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
